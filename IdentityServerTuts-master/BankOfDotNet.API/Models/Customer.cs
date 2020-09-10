80c63641bb5150ee26ea4db57abac70644aecc9d
kofijim <40745396+kofijim@users.noreply.github.com>
2020-09-10 09:59:42 +0000

    Add files via upload
    First Commit

---
+++ IdentityServerTuts-master/BankOfDotNet.API/Models/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace BankOfDotNet.API.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ IdentityServerTuts-master/BankOfDotNet.IdentityServer/Config.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using IdentityServer4.Models;
+
+namespace BankOfDotNet.IdentityServer
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetAllApiResources => new List<ApiResource>
+        {
+            new ApiResource(
+                "BankOfDotNetApi","Customer API for Bank of Dot net")
+        };
+
+        public static IEnumerable<Client> GetAllClients=>new List<Client>
+        {
+            new Client
+            {
+                ClientId = "client",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets = new List<Secret>
+                {
+                    new Secret("secret".Sha256())
+                },
+                AllowedScopes = new List<string>
+                {
+                    "BankOfDotNetApi"
+                }
+            }
+        };
+    }
+}
